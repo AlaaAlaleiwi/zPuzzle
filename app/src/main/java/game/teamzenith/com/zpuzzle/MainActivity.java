59258aa935526627f4ed190c1d23906884bd5aca
Shubha Rao <shubha.rao@hotmail.com>
2017-03-13 11:45:53 +0100

    Checking in initial project structure

---
+++ app/src/main/java/game/teamzenith/com/zpuzzle/MainActivity.java
@@ -0,0 +1,13 @@
+package game.teamzenith.com.zpuzzle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
